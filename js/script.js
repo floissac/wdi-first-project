817e92e143c3d25ad6b3a62a8144ca0d0e18c9ea
floissac <floissac.w@gmail.com>
2017-12-12 15:29:07 +0000

    fixed .buttons css and dropdown.hide

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ $(() => {
     $dropdown.toggle();
     console.log('drop');
 
+
   }
 
   function characterSelection(e){
@@ -22,25 +23,77 @@ $(() => {
     const $img = $(`<img src="images/${choice}.png">`);
     $characterSelection.append($img);
     console.log(choice);
-    $dropdownContent.hide();
     $playerBars.show();
+    $dropbtn.hide();
+    $dropdown.hide();
   }
 
-
-
+  // <----------Game Logic---------->
 
   let yourHealth = 100;
   let compHealth = 100;
-  const $yourHealthbar = $('#yourHealthBar');
+  const $yourHealthBar = $('#yourHealthBar');
   const $compHealthBar = $('#compHealthBar');
   const $attackButton = $('#attack');
   const $buttons = $('.buttons');
+
+
+  // <------invoke fight---->
+
+
+
+  // <----change Health-------->
+  function healthChange() {
+    $yourHealthBar.css('width', yourHealth + '%')
+    $compHealthBar.css('width', compHealth + '%')
+
+  }
+
+  // <-----Attack Select---------->
 
   $buttons.on('click', attackSelect);
 
   function attackSelect(){
     $attackButton.toggle();
     console.log('attack');
+    $dropdownContent.hide();
+    $dropbtn.hide();
+    compMove();
+    healthChange();
   }
 
+  function compMove(id) {
+    const move = Math.floor((Math.random()*4)+1);
+    if (move <= 3) {
+      const savedCompMove = 'attack';
+      compDamage(id, savedCompMove);
+      yourDamage();
+    }
+  }
+
+  // <----------Health Damage------------>
+
+  function compDamage(y, c) {
+    var move = Math.floor((Math.random()*4)+1);
+    if ( y === 'attack' && c === 'attack' && compHealth >= 10) {
+      compHealth -= 10;
+
+    } else if (move >= 3 && y === 'attack'); {
+      compHealth -= Math.floor((Math.random()*4)+1);
+    }
+  }
+
+  function yourDamage(d, e) {
+    var move = Math.floor((Math.random()*4)+1);
+    if ( d === 'attack' && e === 'attack' && yourHealth >= 10) {
+      yourHealth -= 10;
+
+    } else if (move >= 3 && e === 'attack'); {
+      yourHealth -= Math.floor((Math.random()*4)+1);
+    }
+    // if (yourHealth <= 1); {
+    //   alert('loser!');
+    // }
+
+  }
 });
